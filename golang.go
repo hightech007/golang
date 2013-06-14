c13ad5e4ee28460ddd884132338340456a5684ff
Tuku Man <mathphobia@gmail.com>
2013-06-14 10:11:04 -0400

    sqrt from package newmath

--- golang.go
+++ golang.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/straycatcoder/newmath"
+)
 
 func main() {
 	fmt.Printf("Hello, world.\n")
+	fmt.Printf("Sqrt(2) =  %v\n", newmath.Sqrt(2))
 }
 
